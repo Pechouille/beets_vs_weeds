442100de56fa44bb447840e50023d6e6276a1bd8
Quentin Ramoisiaux <quentin.ramoisiaux@gmail.com>
2025-06-03 15:46:08 +0200

    preprocess et model cnn

---
+++ weeds_detector/ml_logic/preprocess.py
@@ -0,0 +1,46 @@
+from weeds_detector.params import *
+
+from PIL import Image
+
+import os
+import numpy as np
+import pandas as pd
+
+from torchvision import transforms
+
+from weeds_detector.utils.padding import expand2square
+
+
+def preprocess_features(X, output_folder):
+    # X est le folder où se trouvent les images à traiter
+    # Output folder est le folder où rajouter les images traitées
+    
+    list_of_tensors = []
+    transform = transforms.Compose([transforms.PILToTensor()])
+
+    for image_name in os.listdir(X):
+
+        image_path = os.path.join(X, image_name)
+        img = Image.open(image_path).convert("RGB")
+
+        new_image = expand2square(img, (0, 0, 0)).resize((128,128))
+        save_path = os.path.join(output_folder, image_name)
+
+        new_image.save(save_path)
+
+        transf = transform(new_image)
+        tensor = transf.permute(1, 2, 0)
+        list_of_tensors.append(tensor)
+
+    X_train = np.array([tensor.numpy() for tensor in list_of_tensors])
+    X_train = X_train / 255
+
+    y = np.zeros(1000)
+    i = -1
+    for image_name in os.listdir(X):
+        i += 1
+        if image_name[-5] == '1':
+            y[i] = 1
+    y_train = pd.Series(y)
+
+    return X_train, y_train
---
+++ weeds_detector/utils/padding.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+def expand2square(pil_img, background_color):
+    # effectue une opération de padding pour rendre une image carrée, tout en conservant son ratio d’origine
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
