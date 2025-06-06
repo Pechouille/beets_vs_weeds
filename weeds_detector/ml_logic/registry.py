df03d8787fddf30e115df9de6bf9d919fe87caa9
Quentin Ramoisiaux <quentin.ramoisiaux@gmail.com>
2025-06-06 12:16:30 +0200

    load and save model functions

--- weeds_detector/ml_logic/registry.py
+++ weeds_detector/ml_logic/registry.py
@@ -3,12 +3,14 @@ import os
 from weeds_detector.params import *
 from tensorflow import keras
 from google.cloud import storage
+from colorama import Fore, Style
+import glob
+
 
 def save_model(model: keras.Model = None) -> None:
     """
     Persist trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}/models/{timestamp}.h5"
     - if MODEL_TARGET='gcs', also persist it in your bucket on GCS at "models/{timestamp}.h5" --> unit 02 only
-    - if MODEL_TARGET='mlflow', also persist it on MLflow instead of GCS (for unit 0703 only) --> unit 03 only
     """
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
@@ -21,7 +23,7 @@ def save_model(model: keras.Model = None) -> None:
 
     if MODEL_TARGET == "gcs":
 
-        model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.h5" for instance
+        model_filename = model_path.split("/")[-1]
         client = storage.Client()
         bucket = client.bucket(BUCKET_NAME)
         blob = bucket.blob(f"models/{model_filename}")
@@ -32,3 +34,47 @@ def save_model(model: keras.Model = None) -> None:
         return None
 
 
+def load_model():
+
+        if MODEL_TARGET == "local":
+
+            print(Fore.BLUE + f"\nLoad latest model from local registry..." + Style.RESET_ALL)
+
+            # Get the latest model version name by the timestamp on disk
+            local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, "models")
+            local_model_paths = glob.glob(f"{local_model_directory}/*")
+
+            if not local_model_paths:
+                return None
+
+            most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
+
+            print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
+
+            latest_model = keras.models.load_model(most_recent_model_path_on_disk)
+
+            print("✅ Model loaded from local disk")
+
+            return latest_model
+
+        elif MODEL_TARGET == "gcs":
+
+            print(Fore.BLUE + f"\nLoad latest model from GCS..." + Style.RESET_ALL)
+
+            client = storage.Client()
+            blobs = list(client.get_bucket(BUCKET_NAME).list_blobs(prefix="model"))
+
+            try:
+                latest_blob = max(blobs, key=lambda x: x.updated)
+                latest_model_path_to_save = os.path.join(LOCAL_REGISTRY_PATH, latest_blob.name)
+                latest_blob.download_to_filename(latest_model_path_to_save)
+
+                latest_model = keras.models.load_model(latest_model_path_to_save)
+
+                print("✅ Latest model downloaded from cloud storage")
+
+                return latest_model
+            except:
+                print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
+
+                return None
--- weeds_detector/params.py
+++ weeds_detector/params.py
@@ -31,6 +31,4 @@ for env, valid_options in env_valid_options.items():
     validate_env_value(env, valid_options)
 
 ##################  CONSTANTS  #####################
-LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), ".code", "Pechouille", "beets_vs_weeds", "training_outputs")
-LOCAL_REGISTRY_PATH =os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
-    "training_outputs")
+LOCAL_REGISTRY_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "training_outputs")
