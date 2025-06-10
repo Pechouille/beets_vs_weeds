2befc16e50fd7de6ca631d9db61433a78800119e
Quentin Ramoisiaux <quentin.ramoisiaux@gmail.com>
2025-06-10 16:49:57 +0200

    ok

--- weeds_detector/ml_logic/registry.py
+++ weeds_detector/ml_logic/registry.py
@@ -55,14 +55,15 @@ def load_model(model_type: str):
             return latest_model
 
         elif MODEL_TARGET == "gcs":
+
             print(Fore.BLUE + f"\nLoad latest model from GCS..." + Style.RESET_ALL)
+            
             client = storage.Client()
             blobs = list(client.get_bucket(BUCKET_NAME).list_blobs(prefix="model"))
-            latest_blob = max(blobs, key=lambda x: x.updated)
 
+            latest_blob = max(blobs, key=lambda x: x.updated)
             latest_model_path_to_save = os.path.join(LOCAL_REGISTRY_PATH, latest_blob.name)
             latest_blob.download_to_filename(latest_model_path_to_save)
-
             latest_model = keras.models.load_model(latest_model_path_to_save)
 
             print("✅ Latest model downloaded from cloud storage")
