a8c3340d438cbc48856ca12d2ddedcdf9e9f8925
Quentin Ramoisiaux <quentin.ramoisiaux@gmail.com>
2025-06-10 18:19:42 +0200

    fast api

--- weeds_detector/api/fast.py
+++ weeds_detector/api/fast.py
@@ -16,12 +16,19 @@ from weeds_detector.utils.image_croping import crop_image
 from weeds_detector.ml_logic.registry import load_model
 from weeds_detector.ml_logic.preprocess_model_class import preprocess_features, preprocess_single_image
 
+from weeds_detector.params import *
+from weeds_detector.utils.padding import expand2square
+
+from tensorflow import expand_dims
+
 # Dossiers pour stocker les images
 UPLOAD_DIR = "data/all/"
 OUTPUT_DIR = "api/outputs/"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 app = FastAPI()
+app.state.model1 = load_model(model_type = 'cnn_classif')
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -35,29 +42,47 @@ app.add_middleware(
 def root():
     return {"message": "Hello, API is running."}
 
-@app.post("/upload/")
-async def create_upload_file(file: UploadFile = File(...)):
-    filename = file.filename  # Ne pas renommer
-    file_path = os.path.join(UPLOAD_DIR, filename)
+@app.get("/predict")
+def predict():
 
-    with open(file_path, "wb") as f:
-        f.write(await file.read())
+    resized_value = int(RESIZED)
+    X_processed = expand2square(img, (0, 0, 0)).resize((resized_value, resized_value))
+    X_processed = img_to_array(X_processed)
+    X_processed = X_processed / 255.0
+
+    X_processed = expand_dims(X_processed, axis = 0)
+
+    prediction = app.state.model.predict(X_processed)
+
+    return {'category': float(prediction)}
+
+
+
+
+
+
+
+
+#@app.post("/upload/")
+#async def create_upload_file(file: UploadFile = File(...)):
+#    filename = file.filename  # Ne pas renommer
+#    file_path = os.path.join(UPLOAD_DIR, filename)
+#
+#   with open(file_path, "wb") as f:
+#       f.write(await file.read())
 
     # Générer l’image annotée
-    output_filename = f"boxed_{filename}"
-    output_path = os.path.join(OUTPUT_DIR, output_filename)
+#    output_filename = f"boxed_{filename}"
+#   output_path = os.path.join(OUTPUT_DIR, output_filename)
+#
+#   try:
+#       api_display_image_with_bounding_boxes(file_path, save_path=output_path)
+#   except FileNotFoundError:
+#       return {"error": f"Fichier XML non trouvé pour {filename}. Attendait : data/all/{os.path.splitext(filename)[0]}.xml"}
+#   except Exception as e:
+#       return {"error": str(e)}
 
-    try:
-        api_display_image_with_bounding_boxes(file_path, save_path=output_path)
-    except FileNotFoundError:
-        return {"error": f"Fichier XML non trouvé pour {filename}. Attendait : data/all/{os.path.splitext(filename)[0]}.xml"}
-    except Exception as e:
-        return {"error": str(e)}
-
-    return FileResponse(output_path, media_type="image/png")
-
-
-
+#    return FileResponse(output_path, media_type="image/png")
 
 # @app.get("/predict/")
 # async def predict(file: UploadFile = File(...)):
